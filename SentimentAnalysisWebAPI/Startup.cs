3d1852c0cda4ce750d013f6a9da0c5220f07d916
ulfsv <ulf.e.sv@gmail.com>
2020-12-19 13:05:04 +0100

    Swagger added

--- SentimentAnalysisWebAPI/Startup.cs
+++ SentimentAnalysisWebAPI/Startup.cs
@@ -17,6 +17,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.ML;
 using SentimentAnalysisWebAPI.DataModels;
+using System;
+using System.Reflection;
+using System.IO;
+using Microsoft.OpenApi.Models;
 
 namespace SentimentAnalysisWebAPI
 {
@@ -35,6 +39,28 @@ namespace SentimentAnalysisWebAPI
             services.AddControllers();
             services.AddPredictionEnginePool<SentimentData, SentimentPrediction>()
     .FromFile(modelName: "SentimentAnalysisModel", filePath: "MLModels/sentiment_model.zip", watchForChanges: true);
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Sentiment Analysis API",
+                    Description = "A simple sentiment prediction example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Ulf Svensson",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/ulfsv"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +72,20 @@ namespace SentimentAnalysisWebAPI
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
 
             app.UseRouting();
 
